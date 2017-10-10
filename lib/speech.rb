6b9b1dea0e5382614e71e8aa72c70c744cfe86f6
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-10-10 23:07:23 +0200

    added gem tree

---
+++ lib/speech.rb
@@ -0,0 +1,83 @@
+require 'net/http'
+require 'win32/sound'
+include Win32
+
+class Speech
+	GOOGLE_TRANSLATE_URL = 'http://translate.google.com/translate_tts'.freeze
+
+	attr_accessor :text, :lang
+
+	def initialize(text, lang = 'en')
+	  @text = text
+	  @lang = lang
+	end
+
+	def self.load(file_path, lang = 'en')
+	  f = File.open(file_path)
+	  new f.read.encode('UTF-16be', invalid: :replace, replace: '?').encode('UTF-8'), lang
+	end
+
+	def save(file_path)
+	  uri = URI(GOOGLE_TRANSLATE_URL)
+
+	  response = []
+
+	  sentences = text.split(/[,.\r\n]/i)
+	  sentences.reject!(&:empty?)
+	  sentences.map! { |t| divide(t.strip) }.flatten!
+
+	  sentences.each_with_index do |q, _idx|
+		uri.query = URI.encode_www_form(
+		  ie: 'UTF-8',
+		  q: q,
+		  tl: lang,
+		  total: sentences.length,
+		  idx: 0,
+		  textlen: q.length,
+		  client: 'tw-ob',
+		  prev: 'input'
+		)
+
+		res = Net::HTTP.get_response(uri)
+
+		next unless res.is_a?(Net::HTTPSuccess)
+
+		response << res.body.force_encoding(Encoding::UTF_8)
+	  end
+
+	  File.open(file_path, 'wb') do |f|
+		f.write response.join
+		return f.path
+	  end
+	end
+
+	private
+
+	def divide(text)
+	  return text if text.length < 150
+
+	  attempts = text.length / 150.0
+	  starts = 0
+	  arr = []
+
+	  attempts.ceil.times do
+		ends = starts + 150
+		arr << text[starts...ends]
+		starts = ends
+	  end
+
+	  arr
+	end
+end
+
+def speak(text)
+  Speech.new(text).save('temp.wav')
+  #system 'vlc/vlc.exe --intf dummy temp.wav'
+  system 'vlc --play-and-exit --intf dummy temp.wav'
+  #pid = fork{ exec 'mpg123','-q', temp.wav }
+  #Sound.play("temp.wav")
+end
+
+def beep(a, b)
+  Sound.beep(a, b)
+end
