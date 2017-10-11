9da7f38ad650f0f34f78682064784a6df9a0fb0d
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-10-11 10:15:41 +0200

    added recipes

---
+++ recipes/guess_the_number.rb
@@ -0,0 +1,34 @@
+require 'rubysounds'
+
+speak 'Hello, I am Ruby'
+speak 'What is your name?'
+name = gets.chomp
+
+speak 'Nice to meet you, ' + name
+
+speak 'Do you want to play a game?'
+
+if gets.chomp == 'yes'
+
+	# Guess the number game
+	speak 'guess the number between 0 and 99'
+	number = rand(0..99)
+	answer = gets.chomp.to_i
+
+	while answer != number
+		speak 'wrong'
+
+		if answer > number
+			speak 'too high'
+		else
+			speak 'too low'
+		end
+		
+		answer = gets.chomp.to_i
+	end
+
+	speak 'Great! It was' + answer.to_s
+
+end
+
+speak 'Goodbye'
